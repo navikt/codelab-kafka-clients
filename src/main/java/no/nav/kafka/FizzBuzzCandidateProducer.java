cb090f36b82eaa0a66b19b9ecb30c655931801a7
Geir André Lund <geir.andre.lund@nav.no>
2019-02-25 18:15:12 +0100

    FizzBuzzNumberEntered value is an Integer

--- src/main/java/no/nav/kafka/FizzBuzzCandidateProducer.java
+++ src/main/java/no/nav/kafka/FizzBuzzCandidateProducer.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,14 +23,14 @@ public class FizzBuzzCandidateProducer {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
 
-        final KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        final KafkaProducer<String, Integer> producer = new KafkaProducer<>(props);
 
         IntStream.range(1, 11).forEach(n -> {
-            final Number fizzBuzzCandidate = new Random().nextInt(100);
-            //todo - partition key..
-            final ProducerRecord<String, String> record = new ProducerRecord<>("FizzBuzzNumberEntered", fizzBuzzCandidate.toString());
+            final Integer fizzBuzzCandidate = new Random().nextInt(100);
+
+            final ProducerRecord<String, Integer> record = new ProducerRecord<>("FizzBuzzNumberEntered", fizzBuzzCandidate);
 
             //produce record
             producer.send(record, new Callback() {
