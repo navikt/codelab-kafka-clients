4e31779c9d800858183c0eff9a51de50e9f1525a
Geir André Lund <geir.andre.lund@nav.no>
2019-12-10 07:56:43 +0100

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ repositories {
     mavenCentral()
 }
 
-val log4j2Version = "2.11.1"
-val kafkaClientsVersion = "2.0.1"
+val log4j2Version = "2.12.1"
+val kafkaClientsVersion = "2.3.1"
 
 val libs = "$projectDir/libs"
 val implementationLib = "$libs/implementation"
@@ -22,7 +22,7 @@ dependencies {
         implementation(group = "org.apache.logging.log4j", name = "log4j-api", version = log4j2Version)
         implementation(group = "org.apache.logging.log4j", name = "log4j-core", version = log4j2Version)
         implementation(group = "org.apache.logging.log4j", name = "log4j-slf4j-impl", version = log4j2Version)
-        implementation(group = "com.google.code.gson", name = "gson", version = "2.8.5")
+        implementation(group = "com.google.code.gson", name = "gson", version = "2.8.6")
     }
 
 }
